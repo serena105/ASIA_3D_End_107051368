564a609331ff540f6d547012d594bb000d4f479f
serena105 <71679914+serena105@users.noreply.github.com>
2021-01-14 03:05:07 +0800

    1/14-03:05

---
+++ 3D_End_107051368/Assets/Script/NPCData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NPC 資料", menuName = "S/NPC 資料")]
+public class NPCData : MonoBehaviour
+{
+    [Header("第一段"), TextArea(1, 5)]
+    public string dialogA;
+    [Header("第二段"), TextArea(1, 5)]
+    public string dialogB;
+    [Header("第三段"), TextArea(1, 5)]
+    public string dialogC;
+    [Header("需求數量")]
+    public int count;
+    [Header("取得數量")]
+    public int countCurrent;
+
+}
---
+++ 3D_End_107051368/Assets/Script/npc.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine; 
+using UnityEngine.UI;
+using System.Collections;
+
+public class npc : MonoBehaviour
+{
+    public NPCData data;
+    public GameObject dialog;
+    public Text textContent;
+    public Text textName;
+    public float interval = 0.2f;
+
+    public enum npcstate
+    {
+        FirstDialog, Missioning,finish
+    }
+
+    public npcstate state = npcstate.FirstDialog;
+    private bool playerInArea;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Hero")
+        {
+            playerInArea = false;
+            StopDialog();
+        }
+    }
+
+    private void StopDialog()
+    {
+        dialog.SetActive(false);
+        StopAllCoroutines();
+    }
+
+    private IEnumerator Dialog()
+    {
+        dialog.SetActive(true);
+        textContent.text = "";
+        textName.text = name;
+
+        string dialogString = data.dialogA;
+
+        switch (state)
+        {
+            case npcstate.FirstDialog:
+                dialogString = data.dialogA;
+                break;
+            case npcstate.Missioning:
+                dialogString = data.dialogB;
+                break;
+            case npcstate.Finish:
+                dialogString = data.dialogC;
+                break;
+        }
+
+        for (int i = 0; i < dialogString.Length; i++)
+        {
+            textContent.text += dialogString[i] + "";
+            yield return new WaitForSeconds(interval);
+        }
+    }
+}
